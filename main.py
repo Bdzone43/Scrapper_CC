8bea772ccdc51831317ae2c6b9d965fca0d2d036
Bdzone43 <150906861+Bdzone43@users.noreply.github.com>
2025-01-02 11:40:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import os
 
 
 # Inserta aquí tus credenciales de API de Telegram
-api_id = 6666666 # Example : 11670008
-api_hash = 'YOUR HASH' # Example : 33a51w803x0f8e9e025b9ca515e1fa1f
+api_id = 23306415 # Example : 11670008
+api_hash = 'fecb61dcfee0e426cf6ef96cc6f32cbc' # Example : 33a51w803x0f8e9e025b9ca515e1fa1f
 
 scarscapper = "@CHANNEL_SCRAPE_NORMAL"
 vipscrapper = "@CHANNEL_SCRAPE_VIP"
